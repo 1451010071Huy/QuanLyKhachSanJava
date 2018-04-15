66a49f7a8a8c6b03f34aab0885fb3ad3dd194775
Manh Huy <1451010071Huy@ou.edu.vn>
2018-04-15 13:12:28 +0700

    fix

--- src/restaurant/manager/controllers/PhieuThuePhongController.java
+++ src/restaurant/manager/controllers/PhieuThuePhongController.java
@@ -21,7 +21,6 @@ public class PhieuThuePhongController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    }
+
 }
